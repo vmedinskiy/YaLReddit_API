5d24c8d33d437c429d8d951bd726a8a0a0ec5d37
Vladimir Medinskiy <v.medinskiy@corp.mail.ru>
2023-01-04 13:43:59 +0300

    added waitress

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from waitress import serve
 from api.posts import PostListRes, PostRes
 from api.users import RegisterRes, LoginRes, UserPosts
 from app.app import main_app
+import logging
+
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.DEBUG)
 
 
 @main_app.route('/', defaults={'path': ''})
@@ -13,10 +17,10 @@ def root(path):
 
 main_app.api.add_resource(RegisterRes, '/api/register')
 main_app.api.add_resource(LoginRes, '/api/login')
-main_app.api.add_resource(PostListRes, '/api/posts/<category_name>', '/api/posts/')
+main_app.api.add_resource(PostListRes, '/api/posts/<category_name>', '/api/posts', '/api/posts/')
 main_app.api.add_resource(PostRes, '/api/post/<int:post_id>')
 main_app.api.add_resource(UserPosts, "/api/user/<user_login>")
 
 if __name__ == '__main__':
-    # main_app.run()
+    # main_app.run(host="0.0.0.0", port=8000)
     serve(main_app, host="0.0.0.0", port=8000)
