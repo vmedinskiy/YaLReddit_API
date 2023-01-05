e7e78933a0f41c52e603c510bc79a3f1d529f435
Vladimir Medinskiy <v.medinskiy@corp.mail.ru>
2023-01-05 11:26:09 +0300

    changed post order from newest

--- start.sh
+++ start.sh
@@ -13,6 +13,7 @@ if [ ! -d $VIRTUALENV ]; then
   python3 -m venv $VIRTUALENV
 fi
 
+# Install pip into virtual environment
 if [ ! -f $VIRTUALENV/bin/pip ]; then
   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
 fi
@@ -20,5 +21,5 @@ fi
 # Install the requirements
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-# Run a glorious Python 3 server
+# Run your glorious application
 $VIRTUALENV/bin/python3 server.py
